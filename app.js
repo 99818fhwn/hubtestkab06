73cc824a6cd64abab99e336acf366be379cfff56
99818fhwn <fabian_weisser@arburg.com>
2020-03-18 16:28:17 +0100

    Clean

--- app.js
+++ app.js
@@ -3,11 +3,4 @@ const app = require('express')()
 app.get('/', (req, res) => {
   res.send("Hello from Appsody!");
 });
- 
-app.get('/resource', (req, res) => {
-  res.send("Hello from Resource finally");
-});
- 
-
-
 module.exports.app = app;
